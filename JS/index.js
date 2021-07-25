0623385ab320577df6c61c1177bba0517c6045b7
IsaacMurage-softwaredeveloper <isaac.moringa@student.moringaschool.com>
2021-07-25 19:42:57 +0300

    add function to portfolio section

--- JS/index.js
+++ JS/index.js
@@ -16,7 +16,53 @@ $(document).ready(function(){
 // portfolio overlay
 
 $(document).ready(function(){
-    $("#overlay1").hover(function(){
-        
-    })
+    $("#over").hover(function(){
+      $("#overlay1").animate({opacity:1},60)  
+    },
+    function (){
+        $("#overlay1").animate({opacity:0},50)
+    });
+
+    $("#over1").hover(function(){
+        $("#overlay2").animate({opacity:1},60)  
+      },
+      function (){
+          $("#overlay2").animate({opacity:0},50)
+      });
+      $("#over2").hover(function(){
+        $("#overlay3").animate({opacity:1},60)  
+      },
+      function (){
+          $("#overlay3").animate({opacity:0},50)
+      });
+      $("#over3").hover(function(){
+        $("#overlay4").animate({opacity:1},60)  
+      },
+      function (){
+          $("#overlay4").animate({opacity:0},50)
+      });
+      $("#over4").hover(function(){
+        $("#overlay5").animate({opacity:1},60)  
+      },
+      function (){
+          $("#overlay5").animate({opacity:0},50)
+      });
+      $("#over5").hover(function(){
+        $("#overlay6").animate({opacity:1},60)  
+      },
+      function (){
+          $("#overlay6").animate({opacity:0},50)
+      });
+      $("#over6").hover(function(){
+        $("#overlay7").animate({opacity:1},60)  
+      },
+      function (){
+          $("#overlay7").animate({opacity:0},50)
+      });
+      $("#over7").hover(function(){
+        $("#overlay8").animate({opacity:1},60)  
+      },
+      function (){
+          $("#overlay8").animate({opacity:0},50)
+      });
 })
