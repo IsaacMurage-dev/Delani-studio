8782170776760f3e185c437e8336495058d89810
IsaacMurage-softwaredeveloper <isaac.moringa@student.moringaschool.com>
2021-07-25 13:53:45 +0300

    enhance toggle effect on what we do section

---
+++ JS/index.js
@@ -0,0 +1,14 @@
+// section 1 of jquery(toggle)
+$(document).ready(function(){
+    $("#d1").click(function(){
+        $(".d2").toggle()
+    });
+    $("#dev1").click(function(){
+        $(".dev2").toggle()
+    });
+    $("#prd1").click(function(){
+        $(".prd2").toggle()
+    });
+
+});
+
